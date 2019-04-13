eff328e923378cc6a4f6100b52da65e33528b37e
Dawid <dave201000@gmail.com>
2019-04-13 15:15:50 +0200

    Token works

--- UserManagmentSystem/Controllers/UserController.cs
+++ UserManagmentSystem/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UserManagmentSystem.Models;
@@ -20,6 +21,7 @@ namespace UserManagmentSystem.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public IEnumerable<User> Get()
         {
             return _context.Users;
